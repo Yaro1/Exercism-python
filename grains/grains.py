fa7456fc4c222bcb3ffa91b376b36977a25f1201
Ярослав Поскряков <y.poskryakov@corp.mail.ru>
2021-03-03 20:37:39 +0300

    one more day

--- grains/grains.py
+++ grains/grains.py
@@ -1,6 +1,8 @@
 def square(number):
-    pass
+    if number < 1 or number > 64:
+        raise ValueError("Try another number.")
+    return 2 ** (number - 1)
 
 
 def total():
-    pass
+    return 2**64 - 1
