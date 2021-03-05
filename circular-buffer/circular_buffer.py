f25dc1e419b9d9bdb0f4df5870bb196144708970
Ярослав Поскряков <y.poskryakov@corp.mail.ru>
2021-03-05 14:22:25 +0300

    one more day

--- circular-buffer/circular_buffer.py
+++ circular-buffer/circular_buffer.py
@@ -10,39 +10,41 @@ class CircularBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.data = [""] * capacity
-        self.current_index = 0
-        self.read_index = 0
+        self.current_index = 1
+        self.read_index = 1
         self.is_empty = True
 
     def __len__(self):
         return self.capacity
 
     def read(self):
+        data_index = self.read_index % self.capacity
         if self.is_empty:
             raise BufferEmptyException("The buffer is empty. |uck off.")
-        if self.data[self.read_index % self.capacity] == "":
+        if self.data[data_index] == "":
             raise BufferEmptyException("We can read items only ones.")
-        item = self.data[self.read_index % self.capacity]
-        self.data[self.read_index % self.capacity] = ""
+        item = self.data[data_index]
+        self.data[data_index] = ""
         self.read_index += 1
         return item
 
     def write(self, data):
-        if self.current_index % self.capacity == 0 and self.current_index != 0 and self.data[self.current_index % self.capacity] != "":
+        data_index = self.current_index % self.capacity
+        if data_index == 0 and self.data[data_index] != "":
             raise BufferFullException("The buffer is full. |uck off.")
-        self.data[self.current_index % self.capacity] = data
+        self.data[data_index] = data
         self.current_index += 1
         self.is_empty = False
 
     def overwrite(self, data):
-        if self.current_index >= self.capacity:
+        if self.current_index >= self.capacity + 1:
             self.data[self.read_index % self.capacity] = data
             self.read_index += 1
         else:
             self.write(data)
 
     def clear(self):
-        self.current_index = 0
-        self.read_index = 0
+        self.current_index = 1
+        self.read_index = 1
         self.data = [""] * self.capacity
         self.is_empty = True
