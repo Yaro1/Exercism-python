ff8c109350cef4045e501a621ae758f15ecb91ce
Ярослав Поскряков <y.poskryakov@corp.mail.ru>
2021-03-09 22:18:11 +0300

    one more day

--- matching-brackets/matching_brackets.py
+++ matching-brackets/matching_brackets.py
@@ -1,15 +1,13 @@
-import re
+def is_paired(string):
+    """Check bracket pairing in passed string."""
+    pairs = {'{': '}', '[': ']', '(': ')'}
+    stack = []
 
-brackets = ["(", ")", "[", "]", "{", "}"]
+    for char in string:
+        if char in pairs.keys():
+            stack.append(char)
+        elif char in pairs.values():
+            if not stack or char != pairs[stack.pop()]:
+                return False
 
-
-def check(text):
-    while '()' in text or '[]' in text or '{}' in text:
-        text = text.replace('()', '')
-        text = text.replace('[]', '')
-        text = text.replace('{}', '')
-    return not text
-
-
-def is_paired(input_string):
-    return check(''.join(filter(lambda x: x in brackets, input_string)))
+    return not stack
